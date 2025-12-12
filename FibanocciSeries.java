5d472e61a890e5eff0f3910483214839f0f7d1c9
nandhinikece97 <nandhinikece97@gmail.com>
2025-12-12 15:52:02 +0530

    Add files via upload

---
+++ FibanocciSeries.java
@@ -0,0 +1,23 @@
+
+
+public class FibanocciSeries {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b=1;
+		int i,c;
+		
+		
+		for(i=0; i<=7; i++) {
+			System.out.print(a+",");
+			c=a+b;
+			a=b;
+			b=c;
+			
+			
+			
+		}
+		}
+	}
+
+

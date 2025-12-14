ef9c333a93bd213b7c0aa84939e3d4be66b2db70
nandhinikece97 <nandhinikece97@gmail.com>
2025-12-14 16:13:59 +0530

    Add files via upload

---
+++ IsPrimeNumber.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class IsPrimeNumber {
+
+	   static boolean isPrime(int num) {
+	        if (num <= 1)
+	            return false;
+
+	        for (int i = 2; i <= num / 2; i++) {
+	            if (num % i == 0)
+	                return false;
+	        }
+	        return true;
+	    }
+
+	    public static void main(String[] args) {
+	        int number = 80;
+
+	        if (isPrime(number))
+	            System.out.println(number + " is a Prime number");
+	        else
+	            System.out.println(number + " is a Non-Prime number");
+	    }
+	}
